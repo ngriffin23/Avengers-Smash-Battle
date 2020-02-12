5ff53f197ce21176f4512baa79443031fbe03b63
ngriffin26 <griffin.nikesha@gmail.com>
2020-02-11 19:51:20 -0800

    adds scoreboand display message logic

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
 console.log('sanity check');
+// scoreboard 
+const playerScore = 0;
+const cpuScore = 0;
+const playerScoreId = document.querySelector('#player-score');
+const cpuScoreId = document.querySelector('#cpu-score');
+
+// start screen display
+const messageDisplay = document.querySelector('#message');
+const winnerDisplay = document.querySelector('#winner');
+
+// game characters
+const ironman = document.querySelector('#iron');
+const cyclops = document.querySelector('#cyclops');
+const captainAmerica = document.querySelector('#captain-america');
+const hulk = document.querySelector('#hulk');
+const thanos = document.querySelector('#thanos');
+
+
+
