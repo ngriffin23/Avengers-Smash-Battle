e34f99485fb277e4975aeca365e4e4ce9b01271b
ngriffin26 <griffin.nikesha@gmail.com>
2020-02-11 20:29:08 -0800

    adds function to randomly select characters

--- js/app.js
+++ js/app.js
@@ -16,22 +16,48 @@ const captainAmerica = document.querySelector('#captain-america');
 const hulk = document.querySelector('#hulk');
 const thanos = document.querySelector('#thanos');
 
+// generate  a random number for each character
+function choiceSelection() {
+    const randomNum = Math.floor(Math.random() * 5 );
+ 
+    if(randomNum === 0){
+        messageDisplay.innerHTML = 'Computer selected Iron Man'
+        return 'ironman';
+    } else if (randomNum === 1) {
+        messageDisplay.innerHTML = 'Computer selected Cyclops'
+        return 'cyclops';
+    } else if (randomNum === 2) {
+        messageDisplay.innerHTML = 'Computer selected Captain America';
+        return 'captain america';
+    } else if (randomNum === 3) {
+        messageDisplay.innerHTML = 'Computer selected Hulk'
+        return 'hulk';
+    } else if (randomNum === 4) {
+        messageDisplay.innerHTML  = 'Computer selected Thanos'
+        return 'thanos';
+    }
+}
 
-// event listeners to select characters
-ironman.addEventListener('click', function () {
-    console.log('User clicked Iron man')
+// event listeners to click on characters
+ironman.addEventListener('click', function() {
+    console.log('User clicked Iron man');
+    choiceSelection();
 })
-cyclops.addEventListener('click', function () {
-    console.log('User clicked Cyclops')
+
+cyclops.addEventListener('click', function() {
+    console.log('User clicked Cyclops');
 })
-captainAmerica.addEventListener('click', function () {
-    console.log('User clicked Captain America')
+
+captainAmerica.addEventListener('click', function() {
+    console.log('User clicked Captain America');
 })
-hulk.addEventListener('click', function () {
-    console.log('User clicked Hulk')
+
+hulk.addEventListener('click', function() {
+    console.log('User clicked Hulk');
 })
-thanos.addEventListener('click', function () {
-    console.log('User clicked Thanos')
+
+thanos.addEventListener('click', function() {
+    console.log('User clicked Thanos');
 })
 
 
